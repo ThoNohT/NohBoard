18021a65c6632e72ae40851804b9b9d54567c6d4
Eric <e.c.p.bataille@gmail.com>
2013-05-26 12:20:21 +0200

    v0.7b ready for release

--- nohboard.h
+++ nohboard.h
@@ -27,8 +27,8 @@
 #include <string>
 #include <vector>
 // Version 0xMMmmbb (Major.minor.build)
-#define version 0x000600
-#define version_string L"NohBoard v0.6b"
+#define version 0x000700
+#define version_string L"NohBoard v0.7b"
 #define keyboardVersion 3
 #define configfile L"NohBoard.config"
 
