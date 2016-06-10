5baad8bf11e1035c6730e7e53f63f64f889155b9
ThoNohT <e.c.p.bataille@gmail.com>
2016-06-10 22:00:57 +0200

    Cleanup and version bump to 0.1.1

--- NohBoard/Extra/FileHelper.cs
+++ NohBoard/Extra/FileHelper.cs
@@ -1,11 +1,26 @@
-﻿namespace ThoNohT.NohBoard.Extra
+﻿/*
+Copyright (C) 2016 by Eric Bataille <e.c.p.bataille@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace ThoNohT.NohBoard.Extra
 {
-    using System.Drawing;
     using System.IO;
     using System.Runtime.Serialization.Json;
     using System.Text;
     using System.Xml;
-    using ThoNohT.NohBoard.Keyboard;
 
     public static class FileHelper
     {
